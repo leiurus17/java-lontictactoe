4e833e7a068a67cae443a35af8c689d9b9e69e6d
Marlon <martianworm17@gmail.com>
2018-01-02 11:17:18 +0800

    Initial Commit for the LonTicTacToe

---
+++ src/LonTicTacToe.java
@@ -0,0 +1,130 @@
+/**
+
+	Lon TicTacToe Version 1.0
+	
+	A simple Tic Tac Toe game created in Java.
+
+
+**/
+
+import java.awt. *;
+import java.awt.event.*;
+import java.awt.geom.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class LonTicTacToe
+{
+
+	//Declaring of the components.
+
+	JFrame frame; //The frame that holds the panel and the menu. This is the application's window.
+	
+	JMenuBar menuBar; //The JMenuBar.
+	
+	JMenu menuHelp, menuScore,submenu; //The items on the JMenuBar.
+	
+	JMenuItem menuItemAbout, menuItemScore; //The items of inside the Menu on the JMenuBar i.e. The second level items.
+	
+	LonTicTacToePanel insidePanel1 = new LonTicTacToePanel(); //Calling of the panel to be used inside the JFrame.
+	
+	
+	public LonTicTacToe()
+	{
+	
+		menuBar = new JMenuBar(); //Created an instance of the JMenu
+		
+		
+		//Items of JMenu
+		menuScore = new JMenu("Score");
+		menuScore.setMnemonic(KeyEvent.VK_C);
+		menuScore.getAccessibleContext().setAccessibleDescription("");
+		
+		menuHelp = new JMenu("Help");
+		menuHelp.setMnemonic(KeyEvent.VK_H);
+		menuHelp.getAccessibleContext().setAccessibleDescription("");
+		
+		//Putting the items inside the JMenu
+		menuBar.add(menuScore);
+		menuBar.add(menuHelp);
+		
+		
+		//Item.
+		menuItemScore = new JMenuItem("View Scores",KeyEvent.VK_W);
+		menuItemScore.getAccessibleContext().setAccessibleDescription("");
+		
+		//Putting the item inside the JMenuItem.
+		menuScore.add(menuItemScore);
+		
+		
+		//Item.
+		menuItemAbout = new JMenuItem("About",KeyEvent.VK_T);
+		menuItemAbout.getAccessibleContext().setAccessibleDescription("");
+		
+		//Putting the item inside the JMenuItem.
+		menuHelp.add(menuItemAbout);
+		
+		
+		//Placing an ActionListener so that we can get the click event and do something when the user do clicks the items.
+		menuItemScore.addActionListener(new MenuActionListener());
+		menuItemAbout.addActionListener(new MenuActionListener());
+		
+		
+		//Setting up the frame.
+		frame = new JFrame("Lon TicTacToe Version 1.0");
+		frame.setSize(490,490);
+		
+		//Setting the dimension for the panel.
+		insidePanel1.setPreferredSize(new Dimension(490,490));
+	
+	
+		//Finishing the frame and putting the panel inside the frame.
+		frame.add(insidePanel1);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		
+		
+		//This adds the JMenu to the JFrame
+		frame.setJMenuBar(menuBar);
+		
+		//Packing the frame so that it is okay when viewed.
+		frame.pack();
+		//Just making the frame visible.
+		frame.setVisible(true);
+	
+	
+	}
+	
+	//Here is where we do the ActionListener
+	class MenuActionListener implements ActionListener {
+	
+	//This gets the action and then from that action, we can decide what to do.
+	public void actionPerformed(ActionEvent e) {
+    
+	//If the user clicks the "About."
+	if (e.getSource() == menuItemAbout)
+		{
+		
+			//Then it will show the JOptionPane for some information.
+			JOptionPane.showMessageDialog(frame, "Lon TicTacToe\nCreated by: Marlon Monzon\nCredits to: Justine Afable (Thanks!)\nVersion 1.0\nDate: 4/22/2011\nThis program is created for and inspired by B.","About",JOptionPane.INFORMATION_MESSAGE);
+		}
+	
+	//If the user clicks the "View Scores."
+	if (e.getSource() == menuItemScore)
+		{
+			//Then it will show the JOptionPane for some information.
+			JOptionPane.showMessageDialog(frame, "Testing","Scores",JOptionPane.INFORMATION_MESSAGE);
+	        }
+  }
+}
+	//This is the main
+	public static void main(String[] args)
+	{
+	
+		//Create new instance of this class
+		new LonTicTacToe();
+	
+	}
+
+
+}
---
+++ src/LonTicTacToePanel.java
@@ -0,0 +1,551 @@
+/**
+
+	Lon TicTacToe Version 1.0
+	
+	A simple Tic Tac Toe game created in Java.
+
+
+**/
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class LonTicTacToePanel extends JPanel implements MouseListener
+{
+
+	//Setting up the images.
+	ImageIcon bilog = new ImageIcon("images/ow.jpg");
+	ImageIcon ekis = new ImageIcon("images/eks.jpg");
+	
+	ImageIcon bilogWin = new ImageIcon("images/ow_raw.jpg");
+	ImageIcon ekisWin = new ImageIcon("images/eks_raw.jpg");
+	
+	
+	//This variable determines the current number of turns in the game.
+	int x = 2;
+	
+	
+	
+		//Setting up the JLabels. They are our tiles.
+		JLabel labelPosition1 = new JLabel(); 
+		JLabel labelPosition2 = new JLabel(); 
+		JLabel labelPosition3 = new JLabel(); 
+		JLabel labelPosition4 = new JLabel(); 
+		JLabel labelPosition5 = new JLabel(); 
+		JLabel labelPosition6 = new JLabel(); 
+		JLabel labelPosition7 = new JLabel(); 
+		JLabel labelPosition8 = new JLabel(); 
+		JLabel labelPosition9 = new JLabel();
+
+		
+	//Here I am just painting the Tic Tac Toe lines.
+	public void paint(Graphics g)
+	{
+		
+			Graphics2D g2 = (Graphics2D)g;
+		
+			super.paint(g2);
+			
+			
+			//***Body of the Tic Tac Toe ***//
+			
+			
+			//Outer box.
+			Rectangle2D body = new Rectangle2D.Double(20,20,450,450);
+			g2.setColor(Color.black);
+			g2.setStroke(new BasicStroke(10));
+			g2.draw(body);
+			
+			//Two upper bars.
+			g2.setColor(Color.black);
+			g2.setStroke(new BasicStroke(10));
+			g2.drawLine(170,20,170,470);
+			
+			g2.setColor(Color.black);
+			g2.setStroke(new BasicStroke(10));
+			g2.drawLine(320,20,320,470);
+			
+			//Two horizontal bars.
+			g2.setColor(Color.black);
+			g2.setStroke(new BasicStroke(10));
+			g2.drawLine(20,170,470,170);
+			
+			g2.setColor(Color.black);
+			g2.setStroke(new BasicStroke(10));
+			g2.drawLine(20,320,470,320);
+			
+		
+	}
+	
+	
+	public LonTicTacToePanel()
+	{
+	
+		/*
+	
+		Position 1 (25,25,140,140)
+		Position 2 (175,25,140,140)
+		Position 3 (325,25,140,140)
+		Position 4 (25,175,140,140)
+		Position 5 (175,175,140,140)
+		Position 6 (325,175,140,140)
+		Position 7 (225,325,140,140)
+		Position 8 (175,325,140,140)
+		Position 9 (325,325,140,140)
+	
+		*/
+		
+		setLayout(null); //Needs for the absolute positioning to work.
+		
+		
+
+		// Add mouseListener.
+		labelPosition1.addMouseListener(this);
+		labelPosition2.addMouseListener(this);
+		labelPosition3.addMouseListener(this);
+		labelPosition4.addMouseListener(this);
+		labelPosition5.addMouseListener(this);
+		labelPosition6.addMouseListener(this);
+		labelPosition7.addMouseListener(this);
+		labelPosition8.addMouseListener(this);
+		labelPosition9.addMouseListener(this);
+		
+		
+		 //Image cannot directly be added use this. Needs setLayout(null); first.
+		
+		labelPosition1.setBounds(25,25,140,140); //.setBounds() Used to absolute position a component.
+		labelPosition2.setBounds(175,25,140,140);
+		labelPosition3.setBounds(325,25,140,140);
+		labelPosition4.setBounds(25,175,140,140);
+		labelPosition5.setBounds(175,175,140,140);
+		labelPosition6.setBounds(325,175,140,140);
+		labelPosition7.setBounds(25,325,140,140);
+		labelPosition8.setBounds(175,325,140,140);
+		labelPosition9.setBounds(325,325,140,140);
+		
+		//Adding the labels inside the panel.
+		add(labelPosition1);
+		add(labelPosition2);
+		add(labelPosition3);
+		add(labelPosition4);
+		add(labelPosition5);
+		add(labelPosition6);
+		add(labelPosition7);
+		add(labelPosition8);
+		add(labelPosition9);
+	}
+
+	
+	//Thanks to Justine for fixing / doing this part.
+	@Override
+	public void mouseClicked(MouseEvent evt)
+	{
+		
+		
+		Object o; //Create an object to get the labels.
+		
+		//If the user clicks a label.
+		if ((o = evt.getSource()) instanceof JLabel)
+		{
+		
+			//Then get that label and pass it into tmp.
+			JLabel tmp = (JLabel) o;
+
+			//If tmp has no name or has already had a name of 'ekis' (Why?) and the current turn is even.
+			//I know now why. 8:43PM June 4, 2012
+			if ((tmp.getName()==null || tmp.getName()=="ekis") && x%2==0)
+			{
+			
+				//Then set that label into a circle.
+				tmp.setName("bilog");
+				tmp.setIcon(bilog);
+				
+				x++; //Increment the moves so the next move is odd so that the next image to be placed is the X.
+				
+				//This makes the clicked label unclickable.
+				tmp.removeMouseListener(this);
+				
+				
+			}
+			else
+			{
+				tmp.setName("ekis");
+				tmp.setIcon(ekis);
+				x++;
+				tmp.removeMouseListener(this);
+			}
+		}
+		
+		System.out.println(x);
+		
+				//Winner on top row
+		
+				if ((labelPosition1.getName()=="ekis") && (labelPosition2.getName()=="ekis") && (labelPosition3.getName()=="ekis"))
+				{
+				labelPosition1.setIcon(ekisWin);
+				labelPosition2.setIcon(ekisWin);
+				labelPosition3.setIcon(ekisWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is X!","X Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound(); //Reset the tiles.
+		
+				
+				}
+				
+				else if ((labelPosition1.getName()=="bilog") && (labelPosition2.getName()=="bilog") && (labelPosition3.getName()=="bilog"))
+				{
+				
+				labelPosition1.setIcon(bilogWin);
+				labelPosition2.setIcon(bilogWin);
+				labelPosition3.setIcon(bilogWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is O!","O Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+				
+				}
+				
+				//End of Winner on top row
+				
+				//Winner of middle row
+				
+				else if ((labelPosition4.getName()=="ekis") && (labelPosition5.getName()=="ekis") && (labelPosition6.getName()=="ekis"))
+				{
+				labelPosition4.setIcon(ekisWin);
+				labelPosition5.setIcon(ekisWin);
+				labelPosition6.setIcon(ekisWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is X!","X Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+		
+				
+				}
+				
+				else if ((labelPosition4.getName()=="bilog") && (labelPosition5.getName()=="bilog") && (labelPosition6.getName()=="bilog"))
+				{
+				
+				labelPosition4.setIcon(bilogWin);
+				labelPosition5.setIcon(bilogWin);
+				labelPosition6.setIcon(bilogWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is O!","O Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+				
+				}
+				
+				//End of Winnder of middle row
+				
+				//Winner of bottom row
+				
+				else if ((labelPosition7.getName()=="ekis") && (labelPosition8.getName()=="ekis") && (labelPosition9.getName()=="ekis"))
+				{
+				labelPosition7.setIcon(ekisWin);
+				labelPosition8.setIcon(ekisWin);
+				labelPosition9.setIcon(ekisWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is X!","X Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+		
+				
+				}
+				
+				else if ((labelPosition7.getName()=="bilog") && (labelPosition8.getName()=="bilog") && (labelPosition9.getName()=="bilog"))
+				{
+				
+				labelPosition7.setIcon(bilogWin);
+				labelPosition8.setIcon(bilogWin);
+				labelPosition9.setIcon(bilogWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is O!","O Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+				
+				}
+				
+				//End of Winnder of bottom row
+				
+				
+				///////////////////////////////
+				
+				//Winner on 1st column
+		
+				else if ((labelPosition1.getName()=="ekis") && (labelPosition4.getName()=="ekis") && (labelPosition7.getName()=="ekis"))
+				{
+				labelPosition1.setIcon(ekisWin);
+				labelPosition4.setIcon(ekisWin);
+				labelPosition7.setIcon(ekisWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is X!","X Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+		
+				
+				}
+				
+				else if ((labelPosition1.getName()=="bilog") && (labelPosition4.getName()=="bilog") && (labelPosition7.getName()=="bilog"))
+				{
+				
+				labelPosition1.setIcon(bilogWin);
+				labelPosition4.setIcon(bilogWin);
+				labelPosition7.setIcon(bilogWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is O!","O Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+				
+				}
+				
+				//End of Winner on 1st column
+				
+				//Winner of middle column
+				
+				else if ((labelPosition2.getName()=="ekis") && (labelPosition5.getName()=="ekis") && (labelPosition8.getName()=="ekis"))
+				{
+				labelPosition2.setIcon(ekisWin);
+				labelPosition5.setIcon(ekisWin);
+				labelPosition8.setIcon(ekisWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is X!","X Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+		
+				
+				}
+				
+				else if ((labelPosition2.getName()=="bilog") && (labelPosition5.getName()=="bilog") && (labelPosition8.getName()=="bilog"))
+				{
+				
+				labelPosition2.setIcon(bilogWin);
+				labelPosition5.setIcon(bilogWin);
+				labelPosition8.setIcon(bilogWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is O!","O Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+				
+				}
+				
+				//End of Winnder of middle column
+				
+				//Winner of last column
+				
+				else if ((labelPosition3.getName()=="ekis") && (labelPosition6.getName()=="ekis") && (labelPosition9.getName()=="ekis"))
+				{
+				labelPosition3.setIcon(ekisWin);
+				labelPosition6.setIcon(ekisWin);
+				labelPosition9.setIcon(ekisWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is X!","X Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+		
+				
+				}
+				
+				else if ((labelPosition3.getName()=="bilog") && (labelPosition6.getName()=="bilog") && (labelPosition9.getName()=="bilog"))
+				{
+				
+				labelPosition3.setIcon(bilogWin);
+				labelPosition6.setIcon(bilogWin);
+				labelPosition9.setIcon(bilogWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is O!","O Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+				
+				}
+				
+				//End of Winnder of last column
+				
+				///////////////////////////////
+				
+				//Winner on left diagonal
+		
+				else if ((labelPosition1.getName()=="ekis") && (labelPosition5.getName()=="ekis") && (labelPosition9.getName()=="ekis"))
+				{
+				labelPosition1.setIcon(ekisWin);
+				labelPosition5.setIcon(ekisWin);
+				labelPosition9.setIcon(ekisWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is X!","X Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+		
+				
+				}
+				
+				else if ((labelPosition1.getName()=="bilog") && (labelPosition5.getName()=="bilog") && (labelPosition9.getName()=="bilog"))
+				{
+				
+				labelPosition1.setIcon(bilogWin);
+				labelPosition5.setIcon(bilogWin);
+				labelPosition9.setIcon(bilogWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is O!","O Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+				
+				}
+				
+				//End of Winner on left diagonal
+				
+				//Winner of middle right diagonal
+				
+				else if ((labelPosition3.getName()=="ekis") && (labelPosition5.getName()=="ekis") && (labelPosition7.getName()=="ekis"))
+				{
+				labelPosition3.setIcon(ekisWin);
+				labelPosition5.setIcon(ekisWin);
+				labelPosition7.setIcon(ekisWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is X!","X Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+		
+				
+				}
+				
+				else if ((labelPosition3.getName()=="bilog") && (labelPosition5.getName()=="bilog") && (labelPosition7.getName()=="bilog"))
+				{
+				
+				labelPosition3.setIcon(bilogWin);
+				labelPosition5.setIcon(bilogWin);
+				labelPosition7.setIcon(bilogWin);
+				
+				JOptionPane.showMessageDialog(this, "Winner is O!","O Won!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+				
+				}
+				
+				//End of Winnder of right diagonal
+				
+				//Draw
+		
+				else if (
+				((labelPosition1.getName()=="ekis") || (labelPosition1.getName()=="bilog")) && 
+				((labelPosition2.getName()=="ekis") || (labelPosition2.getName()=="bilog")) &&
+				((labelPosition3.getName()=="ekis") || (labelPosition3.getName()=="bilog")) &&
+				((labelPosition4.getName()=="ekis") || (labelPosition4.getName()=="bilog")) &&
+				((labelPosition5.getName()=="ekis") || (labelPosition5.getName()=="bilog")) &&
+				((labelPosition6.getName()=="ekis") || (labelPosition6.getName()=="bilog")) &&
+				((labelPosition7.getName()=="ekis") || (labelPosition7.getName()=="bilog")) &&
+				((labelPosition8.getName()=="ekis") || (labelPosition8.getName()=="bilog")) &&
+				((labelPosition9.getName()=="ekis") || (labelPosition9.getName()=="bilog"))
+				)
+				{
+				
+				JOptionPane.showMessageDialog(this, "No winner!","Draw!",JOptionPane.INFORMATION_MESSAGE);
+				
+		
+				restartRound();
+		
+				
+				}
+				
+				
+				
+		
+	}
+	
+	//Setting up the tiles in the event of a win or a draw.
+	public void restartRound()
+	{
+		x = 2;
+		
+		//Resetting the labels
+		
+		labelPosition1.removeMouseListener(this);
+		labelPosition2.removeMouseListener(this);
+		labelPosition3.removeMouseListener(this);
+		labelPosition4.removeMouseListener(this);
+		labelPosition5.removeMouseListener(this);
+		labelPosition6.removeMouseListener(this);
+		labelPosition7.removeMouseListener(this);
+		labelPosition8.removeMouseListener(this);
+		labelPosition9.removeMouseListener(this);
+		
+		labelPosition1.setName(null);
+		labelPosition2.setName(null);
+		labelPosition3.setName(null);
+		labelPosition4.setName(null);
+		labelPosition5.setName(null);
+		labelPosition6.setName(null);
+		labelPosition7.setName(null);
+		labelPosition8.setName(null);
+		labelPosition9.setName(null);
+		
+		labelPosition1.setIcon(null);
+		labelPosition2.setIcon(null);
+		labelPosition3.setIcon(null);
+		labelPosition4.setIcon(null);
+		labelPosition5.setIcon(null);
+		labelPosition6.setIcon(null);
+		labelPosition7.setIcon(null);
+		labelPosition8.setIcon(null);
+		labelPosition9.setIcon(null);
+	
+		labelPosition1.addMouseListener(this);
+		labelPosition2.addMouseListener(this);
+		labelPosition3.addMouseListener(this);
+		labelPosition4.addMouseListener(this);
+		labelPosition5.addMouseListener(this);
+		labelPosition6.addMouseListener(this);
+		labelPosition7.addMouseListener(this);
+		labelPosition8.addMouseListener(this);
+		labelPosition9.addMouseListener(this);
+		
+		this.repaint(); //Repaint the panel.
+	}
+	
+	
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
